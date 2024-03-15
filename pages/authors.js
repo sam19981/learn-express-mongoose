04d9ac0bec1ece23e5a37c4f334b3a345ae14bfd
sankar <rsankar19981@gmail.com>
2024-03-15 17:43:02 -0400

    InClass Activity 031524

--- pages/authors.js
+++ pages/authors.js
@@ -1,7 +1,7 @@
 let Author = require('../models/author');
 
 get_author_list = async () => {
-  let authors_list = [];
+  let authors_list = await Author.find().sort([['family_name', 'ascending']]).exec()
   return authors_list.map(function(author) {
     return author.name + " : " + author.lifespan;
   });
@@ -9,6 +9,6 @@ get_author_list = async () => {
 
 exports.show_all_authors = function(res) {
   get_author_list()
-    .then((data) => res.send(data))
-    .catch((_) => res.send('No authors found'));
+      .then((data) => res.send(data))
+      .catch((_) => res.send('No authors found'));
 }
